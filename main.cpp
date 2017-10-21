6cde8a572e16faf6428147e6e6740c2932645fec
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2017-10-21 12:03:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,36 @@ using namespace std;
  struct complex_t {
       float real;
       float imag;
-    }ch,ch1;
-bool proverka(istringstream & stream,complex_t & ch)
+    }ch1,ch2;
+
+complex_t add(complex_t ch1, complex_t ch2){
+    complex_t ch;
+    ch.real=ch1.real+ch2.real;
+    ch.imag=ch1.imag+ch2.imag;
+    return ch;
+}
+
+complex_t sub(complex_t ch1, complex_t ch2){
+    complex_t ch;
+    ch.real=ch1.real-ch2.real;
+    ch.imag=ch1.imag-ch2.imag;
+    return ch;
+}
+
+complex_t mul(complex_t ch1, complex_t ch2){
+    complex_t ch;
+    ch.real=ch1.real*ch2.real-ch1.imag*ch2.imag;
+    ch.imag=ch1.imag*ch2.real+ch1.real*ch2.imag;
+    return ch;
+}
+
+complex_t div(complex_t ch1, complex_t ch2){
+    complex_t ch;
+    ch.real=(ch1.real*ch2.real+ch1.imag*ch2.imag)/(ch2.real*ch2.real+ch2.imag*ch2.imag);
+    ch.imag=(ch1.imag*ch2.real-ch1.real*ch2.imag)/(ch2.real*ch2.real+ch2.imag*ch2.imag);
+    return ch;
+}
+bool read(istringstream & stream,complex_t & ch)
 { 
     char op1,op2,op3;
     bool f=0;
@@ -57,9 +85,9 @@ int main()
   string stroka;
   getline(cin, stroka);
   istringstream stream(stroka);
-  complex_t ch1;complex_t ch2;
+  complex_t ch1;complex_t ch2;complex_t ch;
   char op;
-  if(proverka(stream,ch1))
+  if(read(stream,ch1))
   {
       stream>>op;
       if((op!='+')&&(op!='-')&&(op!='*')&&(op!='/'))
@@ -70,29 +98,30 @@ int main()
   }
   else {cout<<"An error has occured while reading input data"<<endl;
   return -1;}
-  if(proverka(stream,ch2))
+  if(read(stream,ch2))
   {
       if (op=='+')
       {
-          cout<<"("<<ch1.real+ch2.real<<","<<ch1.imag+ch2.imag<<")"<<endl;
+          ch=add(ch1,ch2);
       }
       else if (op=='-')
       {
-          cout<<"("<<ch1.real-ch2.real<<","<<ch1.imag-ch2.imag<<")"<<endl;
+          ch=sub(ch1,ch2);
       }
       else if (op=='*')
       {
-          cout<<"("<<ch1.real*ch2.real-ch1.imag*ch2.imag<<","<<ch1.imag*ch2.real+ch1.real*ch2.imag<<")"<<endl;
+          ch=mul(ch1,ch2);
       }
       else if (op=='/')
       {
-          cout<<"("<<(ch1.real*ch2.real+ch1.imag*ch2.imag)/(ch2.real*ch2.real+ch2.imag*ch2.imag)<<","<<(ch1.imag*ch2.real-ch1.real*ch2.imag)/(ch2.real*ch2.real+ch2.imag*ch2.imag)<<")"<<endl;
+          ch=div(ch1,ch2);
       }
   }
     else{
         cout<<"An error has occured while reading input data"<<endl;
         return -1;
     }
+ write(ch);
     return 0;
 }
   
